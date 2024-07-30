60b9e7fad13778171cfdd324936ea15cebe42f08
Christian <111076892+i-christian@users.noreply.github.com>
2024-07-31 00:27:08 +0200

    Feat: - Add backend - Config Docker for easier & unified dev

--- NOTES.md
+++
@@ -1,5 +0,0 @@
-- Project client now uses SolidJS with TS from ReactJS
-- TailwindCSS still the choice for styling
-- Routing has been setup
-- Design and implementation of the HomePage comes next
-
---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { Route, Router } from '@solidjs/router';
+import { Component, lazy, ParentComponent } from 'solid-js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+
+const Login: Component = lazy(() => import('./pages/login'));
+const Home: Component = lazy(() => import('./pages/home'));
+const Admin: Component = lazy(() => import('./pages/admin'));
+const Teachers: Component = lazy(() => import('./pages/teachers'));
+const Grades: Component = lazy(() => import('./pages/grades'));
+const NotFound: Component = lazy(() => import('./pages/not_found'));
+
+
+const Layout: ParentComponent = (props) => {
+  return (
+    <main class="mx-auto sm:w-3/4">
+      <Header />
+      <Navigation />
+      {props.children}
+      <Footer />
+    </main>
+  )
+}
+
+
+const App: Component = () => {
+  return (
+    <Router root={Layout}>
+      <Route path="/" component={Home} />
+      <Route path="/admin" component={Admin}>
+        <Route />
+      </Route>
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/login" component={Login} />
+      <Route path="/grades" component={Grades} />
+      <Route path="*404" component={NotFound} />
+    </Router>
+  )
+}
+
+export default App;
