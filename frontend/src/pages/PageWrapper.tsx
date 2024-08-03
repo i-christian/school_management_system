080b9ba5184f96d96f5a52fa3472882f10f0061b
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-03 16:10:01 +0200

    UI: - hide the open sidebar button in desktop mode

--- frontend/src/pages/PageWrapper.tsx
+++ frontend/src/pages/PageWrapper.tsx
@@ -67,9 +67,9 @@ const PageWrapper: ParentComponent = (props) => {
       <main
         class="flex-grow transition-all duration-500 overflow-y-auto lg:ml-64"
       >
-        <header class="flex justify-between mx-5 p-2 lg:hidden">
+        <header class="flex justify-between mx-5 p-2">
           <button
-            class="cursor-pointer"
+            class="cursor-pointer lg:hidden"
             onClick={() => setOpen(!open())}
             aria-label="Open Sidebar"
           >
@@ -82,7 +82,7 @@ const PageWrapper: ParentComponent = (props) => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
         </header>
-        <section class="bg-white w-full p-5">
+        <section class="w-full p-5">
           {props.children}
         </section>
       </main>
