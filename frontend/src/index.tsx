60611d48ee5a25c45f815eb660e516293f96da36
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-21 13:47:11 +0200

    update index.tsx: - Add the backend URL to the app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from "solid-js/web";
 import App from "./App.tsx";
+import { OpenAPI } from "./client/index.ts";
+
+OpenAPI.BASE = import.meta.env.VITE_API_URL
+OpenAPI.TOKEN = async () => {
+  return localStorage.getItem("access_token") || ""
+}
 
 render(
   App, document.getElementById("root")!
