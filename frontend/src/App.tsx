9fbd811aeba87155a29a253fa5a9da023b378015
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-22 14:59:12 +0200

    Feature: implemented the protected routes functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { Logout } from './pages/Logout';
 const Admin: Component = lazy(() => import('./pages/users/Admin'));
 const Home: Component = lazy(() => import('./pages/Home'));
 const WrongPage: Component = lazy(() => import('./pages/404'));
-const PageWrapper: Component = lazy(() => import('./pages/PageWrapper'));
+const Protected: Component = lazy(() => import('./pages/Protected'));
 const Users: Component = lazy(() => import('./pages/users/Users'));
 const User: Component = lazy(() => import('./pages/users/User'));
 const UserSettings: Component = lazy(() => import('./pages/users/UserSettings'));
@@ -23,17 +23,17 @@ const App: Component = () => {
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
-      <Route path="/admin" component={PageWrapper}>
+      <Route path="/admin" component={Protected}>
         <Route path="/" component={Admin} />
       </Route>
-      <Route path="/users" component={PageWrapper}>
+      <Route path="/users" component={Protected}>
         <Route path="/" component={Users} />
         <Route path="/:id" component={User} />
       </Route>
-      <Route path="/settings" component={PageWrapper} >
+      <Route path="/settings" component={Protected} >
         <Route path="/" component={UserSettings} />
       </Route>
-      <Route path="/students" component={PageWrapper}>
+      <Route path="/students" component={Protected}>
         <Route path="/" component={Students} />
         <Route path="/:id" component={Student} />
       </Route>
