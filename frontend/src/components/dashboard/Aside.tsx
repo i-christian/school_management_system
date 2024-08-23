019deb39721c2a48207e5e09eeae7f3e0622795d
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-23 14:26:13 +0200

    update Aside.tsx
    Former-commit-id: 2ec89959d495e1c5770ea1991be894e121cff59b

--- frontend/src/components/dashboard/Aside.tsx
+++ frontend/src/components/dashboard/Aside.tsx
@@ -1,73 +1,89 @@
 import { A } from "@solidjs/router";
-import { Accessor, Component, For, Setter } from "solid-js"
-
+import { Accessor, Component, For, Setter, createSignal } from "solid-js";
+import { useAuth } from "../../context/UserContext";
 
 const Aside: Component<{ open: Accessor<boolean>, setOpen: Setter<boolean> }> = (props) => {
-  const menus = [
+  const { isAuthenticated, user } = useAuth();
+
+  const [menus] = createSignal([
     {
-      name: "Home", link: "/", icon: "m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+      name: "Home",
+      link: "/",
+      icon: "m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25",
     },
-    { name: "Admin", link: "/admin", icon: "M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" },
-    { name: "Users", link: "/users", icon: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" },
-    { name: "Students", link: "/students", icon: "M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" },
-    { name: "User Settings", link: "/settings", icon: "M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" },
-  ];
+    ...(isAuthenticated() && user()?.is_superuser ? [
+      {
+        name: "Admin",
+        link: "/admin",
+        icon: "M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z",
+      },
+      {
+        name: "Users",
+        link: "/users",
+        icon: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z",
+      }
+    ] : []),
+    {
+      name: "Students",
+      link: "/students",
+      icon: "M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5",
+    },
+    {
+      name: "User Settings",
+      link: "/settings",
+      icon: "M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75",
+    },
+  ]);
 
   return (
-    <>
-      <aside
-        class={`bg-slate-500 dark:bg-slate-950 fixed left-0 top-0 shadow-3xl p-5 h-screen ${props.open() ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 transition-transform duration-500 text-gray-100 px-4 w-64 z-50 flex flex-col`}
-      >
-        <header class="flex justify-end lg:hidden">
-          <button
-            class="cursor-pointer"
-            onClick={() => props.setOpen(!props.open())}
-            aria-label="Close Sidebar"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-            </svg>
-          </button>
-        </header>
-        <nav class="flex-grow flex flex-col mt-10 gap-5 text-black dark:text-slate-100">
-          <For each={menus}>
-            {(menu, i) => (
-              <A
-                href={menu.link}
-                class="group flex items-center gap-3.5 font-bold p-2 hover:bg-gray-300 rounded-lg"
-
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                  <path stroke-linecap="round" stroke-linejoin="round" d={menu.icon} />
-                </svg>
-                <span
-                  style={{
-                    "transition-delay": `${i() * 100}ms`,
-                  }}
-                  class="whitespace-pre duration-500"
-                >
-                  {menu.name}
-                </span>
-              </A>
-            )}
-          </For>
-          <section class="mt-auto text-2xl">
-            <hr class="my-5" />
+    <aside
+      class={`bg-slate-500 dark:bg-slate-950 fixed left-0 top-0 shadow-3xl p-5 h-screen ${props.open() ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 transition-transform duration-500 text-gray-100 px-4 w-64 z-50 flex flex-col`}
+    >
+      <header class="flex justify-end lg:hidden">
+        <button
+          class="cursor-pointer"
+          onClick={() => props.setOpen(!props.open())}
+          aria-label="Close Sidebar"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+          </svg>
+        </button>
+      </header>
+      <nav class="flex-grow flex flex-col mt-10 gap-5 text-black dark:text-slate-100">
+        <For each={menus()}>
+          {(menu, i) => (
             <A
-              href="/logout"
-              class="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-700 hover:text-white rounded-md"
+              href={menu.link}
+              class="group flex items-center gap-3.5 font-bold p-2 hover:bg-gray-300 rounded-lg"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d={menu.icon} />
               </svg>
-              <span class="text-xl whitespace-pre duration-500">Sign Out</span>
+              <span
+                style={{ "transition-delay": `${i() * 100}ms` }}
+                class="whitespace-pre duration-500"
+              >
+                {menu.name}
+              </span>
             </A>
-          </section>
-        </nav>
-      </aside>
-
-    </>
-  )
-}
+          )}
+        </For>
+        <section class="mt-auto text-2xl">
+          <hr class="my-5" />
+          <A
+            href="/logout"
+            class="group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-700 hover:text-white rounded-md"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+            </svg>
+            <span class="text-xl whitespace-pre duration-500">Sign Out</span>
+          </A>
+        </section>
+      </nav>
+    </aside>
+  );
+};
 
 export default Aside;
