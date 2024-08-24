4beade13b43fd17183edcca50af42129012630c1
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-24 15:16:21 +0200

    Refactoring dashboard
    Former-commit-id: a73a08c59e837fd707626428f659c4a82f700a10

--- frontend/src/context/index.ts
+++ frontend/src/context/index.ts
@@ -3,13 +3,7 @@ import {
   homeIcon,
   aboutIcon,
   contactIcon,
-  userSettingsIcon,
-  adminIcon,
-  studentsIcon,
-  usersIcon,
   logoutIcon,
-  teachersIcon,
-  reportsIcon,
 } from "../assets/icons";
 
 export const navbarElements = [
@@ -18,20 +12,6 @@ export const navbarElements = [
   { name: "Contact", link: "#contact", icon: contactIcon },
 ];
 
-export const admindashboardElements = [
-  { name: "Home", link: "/", icon: homeIcon },
-  { name: "Admin", link: "/admin", icon: adminIcon },
-  { name: "Users", link: "/users", icon: usersIcon },
-  { name: "Students", link: "/students", icon: studentsIcon },
-  { name: "Teachers", link: "/teachers", icon: teachersIcon },
-  { name: "Reports", link: "/reports", icon: reportsIcon },
-  { name: "User Settings", link: "/settings", icon: userSettingsIcon },
-];
-export const userDashboardElements = [
-  { name: "Home", link: "/", icon: homeIcon },
-  { name: "Students", link: "/students", icon: studentsIcon },
-  { name: "User Settings", link: "/settings", icon: userSettingsIcon },
-];
 export const logOutElement = {
   name: "Sign Out",
   link: "/logout",
