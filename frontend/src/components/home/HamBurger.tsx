a776dd572fb4761875781b190213dca1c5f08cae
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-28 17:50:21 +0200

    update the hamburger button background in light mode
    Former-commit-id: 954212270e4d43e50a6fe3821b35a9720b88b600

--- frontend/src/components/home/HamBurger.tsx
+++ frontend/src/components/home/HamBurger.tsx
@@ -33,35 +33,29 @@ const HamBugerMenuIcon: Component<{
         >
           <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
             <div
-              class={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
-                props.isFocused ? "translate-y-6" : ""
-              } delay-100`}
+              class={`bg-gray-900 dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${props.isFocused ? "translate-y-6" : ""
+                } delay-100`}
             ></div>
             <div
-              class={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 ${
-                props.isFocused ? "translate-y-6" : ""
-              } delay-75`}
+              class={`bg-gray-900 dark:bg-white h-[2px] w-7 rounded transform transition-all duration-300 ${props.isFocused ? "translate-y-6" : ""
+                } delay-75`}
             ></div>
             <div
-              class={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
-                props.isFocused ? "translate-y-6" : ""
-              }`}
+              class={`bg-gray-900 dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${props.isFocused ? "translate-y-6" : ""
+                }`}
             ></div>
 
             <div
-              class={`absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 ${
-                props.isFocused ? "translate-x-0 flex w-12" : "w-0"
-              }`}
+              class={`absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 ${props.isFocused ? "translate-x-0 flex w-12" : "w-0"
+                }`}
             >
               <div
-                class={`absolute bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 ${
-                  props.isFocused ? "rotate-45" : ""
-                }`}
+                class={`absolute bg-gray-900 dark:bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 ${props.isFocused ? "rotate-45" : ""
+                  }`}
               ></div>
               <div
-                class={`absolute bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 ${
-                  props.isFocused ? "-rotate-45" : ""
-                }`}
+                class={`absolute bg-gray-900 dark:bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 ${props.isFocused ? "-rotate-45" : ""
+                  }`}
               ></div>
             </div>
           </div>
