501fa25950b7a5f0404236bed068d1dc699b59d6
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-31 14:30:32 +0200

    feat
    - alembic migrations to use new database tables

---
+++ backend/app/alembic/versions/ac244af23f63_new_system.py
@@ -0,0 +1,25 @@
+"""new_system
+
+Revision ID: ac244af23f63
+Revises: 1a31ce608336
+Create Date: 2024-08-31 14:14:35.749879
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision = 'ac244af23f63'
+down_revision = '1a31ce608336'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
