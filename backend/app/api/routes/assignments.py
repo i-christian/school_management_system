b2f37ff95e33910efcc42ffdcb20ee943fa45fd7
Christian <111076892+i-christian@users.noreply.github.com>
2024-08-31 03:17:07 +0200

    add assignments.py
    Former-commit-id: 19956e58fa1b49f120f9ee8f8492e55aa4918dfc

---
+++ backend/app/api/routes/assignments.py
@@ -0,0 +1,77 @@
+import uuid
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
+from sqlmodel import func, select
+
+from app import crud
+from app.api.deps import SessionDep
+from app.models import (
+    Assignment,
+    AssignmentCreate,
+    AssignmentPublic,
+    AssignmentsPublic,
+    AssignmentUpdate,
+    Message,
+)
+
+router = APIRouter()
+
+
+@router.get("/", response_model=AssignmentsPublic)
+def read_assignments(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
+    """
+    Retrieve assignments.
+    """
+    count_statement = select(func.count()).select_from(Assignment)
+    count = session.exec(count_statement).one()
+    statement = select(Assignment).offset(skip).limit(limit)
+    assignments = session.exec(statement).all()
+    return AssignmentsPublic(data=assignments, count=count)
+
+
+@router.get("/{id}", response_model=AssignmentPublic)
+def read_assignment(id: uuid.UUID, session: SessionDep) -> Any:
+    """
+    Get assignment by ID.
+    """
+    assignment = session.get(Assignment, id)
+    if not assignment:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    return assignment
+
+
+@router.post("/", response_model=AssignmentPublic)
+def create_assignment(*, session: SessionDep, assignment_in: AssignmentCreate) -> Any:
+    """
+    Create new assignment.
+    """
+    assignment = crud.create_assignment(session=session, assignment_in=assignment_in)
+    return assignment
+
+
+@router.put("/{id}", response_model=AssignmentPublic)
+def update_assignment(
+    *, session: SessionDep, id: uuid.UUID, assignment_in: AssignmentUpdate
+) -> Any:
+    """
+    Update an assignment.
+    """
+    assignment = session.get(Assignment, id)
+    if not assignment:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    assignment = crud.update_assignment(
+        session=session, db_assignment=assignment, assignment_in=assignment_in
+    )
+    return assignment
+
+
+@router.delete("/{id}", response_model=Message)
+def delete_assignment(*, session: SessionDep, id: uuid.UUID) -> Message:
+    """
+    Delete an assignment.
+    """
+    assignment = crud.delete_assignment(session=session, assignment_id=id)
+    if not assignment:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    return Message(message="Assignment deleted successfully")
