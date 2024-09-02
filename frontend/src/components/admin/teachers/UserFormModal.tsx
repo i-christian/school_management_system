2863787ed3d0c66c8d26b5020f82419a4ebb8a1e
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-02 16:32:19 +0200

    Update
    - New user management UI
    
    
    Former-commit-id: 42bc6e58015010f0833e2ddcc8e3ac6815879788

--- frontend/src/components/admin/teachers/UserFormModal.tsx
+++ frontend/src/components/admin/teachers/UserFormModal.tsx
@@ -16,6 +16,7 @@ const UserFormModal: Component<UserFormModalProps> = (props) => {
   const [fullName, setFullName] = createSignal("");
   const [isActive, setIsActive] = createSignal(true);
   const [isClassTeacher, setIsClassTeacher] = createSignal(false);
+  const [isAccountant, setIsAccountant] = createSignal(false);
   const [loading, setLoading] = createSignal(false);
 
   const { validateEmailOrPhone, validatePassword } = useValidation();
@@ -36,6 +37,7 @@ const UserFormModal: Component<UserFormModalProps> = (props) => {
         is_active: isActive(),
         is_class_teacher: isClassTeacher(),
         is_superuser: false,
+        is_accountant: isAccountant(),
       },
     };
 
@@ -52,152 +54,181 @@ const UserFormModal: Component<UserFormModalProps> = (props) => {
   };
 
   return (
-    <div class="fixed bg-inherit inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div class="p-6 rounded shadow-md w-96 bg-slate-300 dark:bg-slate-800 relative">
-        <h3 class="text-lg font-bold mb-4 text-gray-600 dark:text-gray-200">New User</h3>
+        <h3 class="text-lg font-bold mb-4 text-gray-600 dark:text-gray-200">Add Users</h3>
+
         <div class="mb-4">
           <label class="block text-sm font-medium mb-2 text-gray-600 dark:text-gray-200" for="fullName">Full Name</label>
           <input
             id="fullName"
             type="text"
             class="border rounded p-2 w-full bg-white text-gray-700 dark:bg-gray-800 dark:text-gray-100"
+            placeholder="Full Name"
             value={fullName()}
             onInput={(e) => setFullName(e.currentTarget.value)}
           />
         </div>
+
         <div class="mb-4">
           <label class="block text-sm font-medium mb-2 text-gray-600 dark:text-gray-200" for="emailOrPhone">Email or Phone</label>
           <input
             id="emailOrPhone"
             type="text"
+            placeholder="Email or Phone"
             class="border rounded p-2 w-full bg-white text-gray-700 dark:bg-gray-800 dark:text-gray-100"
             value={emailOrPhone()}
             onInput={(e) => setEmailOrPhone(e.currentTarget.value)}
           />
         </div>
-        <div class="w-full mt-4 relative">
-          <input
-            id="password"
-            type={showPassword() ? "text" : "password"}
-            class="block w-full px-4 py-2 text-gray-700 dark:text-gray-100 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300 transition duration-300 ease-in-out"
-            placeholder="Password"
-            aria-label="Password"
-            value={password()}
-            onInput={(e) => setPassword(e.currentTarget.value)}
-            required
-          />
-          <button
-            type="button"
-            class="absolute inset-y-0 right-0 px-3 py-2 flex items-center text-gray-600 dark:text-gray-300 hover:text-gray-400 transition duration-300 ease-in-out"
-            onClick={() => setShowPassword(!showPassword())}
-            aria-label={showPassword() ? "Hide Password" : "Show Password"}
-          >
-            {showPassword() ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M13.875 18.825A5 5 0 0 1 5.823 10.8m-.634-1.638A9.88 9.88 0 0 1 12 4c4.572 0 8.573 3.043 10.568 7.27a9.88 9.88 0 0 1-2.206 2.848" />
-                <path d="M15 9a3 3 0 0 0-3 3m0 3a3 3 0 0 0 3-3m-3 0 1.5 1.5" />
-                <line x1="2" y1="2" x2="22" y2="22" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                <circle cx="12" cy="12" r="3" />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div class="w-full mt-4 relative">
-          <input
-            id="confirmPassword"
-            type={showConfirmPassword() ? "text" : "password"}
-            class="block w-full px-4 py-2 text-gray-700 dark:text-gray-100 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300 transition duration-300 ease-in-out"
-            placeholder="Confirm Password"
-            aria-label="Confirm Password"
-            value={confirmPassword()}
-            onInput={(e) => setConfirmPassword(e.currentTarget.value)}
-            required
-          />
-          <button
-            type="button"
-            class="absolute inset-y-0 right-0 px-3 py-2 flex items-center text-gray-600 dark:text-gray-300 hover:text-gray-400 transition duration-300 ease-in-out"
-            onClick={() => setShowConfirmPassword(!showConfirmPassword())}
-            aria-label={showConfirmPassword() ? "Hide Confirm Password" : "Show Confirm Password"}
-          >
-            {showConfirmPassword() ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M13.875 18.825A5 5 0 0 1 5.823 10.8m-.634-1.638A9.88 9.88 0 0 1 12 4c4.572 0 8.573 3.043 10.568 7.27a9.88 9.88 0 0 1-2.206 2.848" />
-                <path d="M15 9a3 3 0 0 0-3 3m0 3a3 3 0 0 0 3-3m-3 0 1.5 1.5" />
-                <line x1="2" y1="2" x2="22" y2="22" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                <circle cx="12" cy="12" r="3" />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div class="my-4 flex gap-2">
-          <label class="block text-sm font-medium text-gray-600 dark:text-gray-200" for="isActive">Active Status: </label>
-          <p class="flex gap-1">
+
+        <div class="mb-4">
+          <label class="block text-sm font-medium mb-2 text-gray-600 dark:text-gray-200" for="password"></label>
+          <div class="relative">
             <input
-              id="isActive"
-              type="checkbox"
-              checked={isActive()}
-              onChange={(e) => setIsActive(e.currentTarget.checked)}
+              id="password"
+              type={showPassword() ? "text" : "password"}
+              class="block w-full px-4 py-2 text-gray-700 dark:text-gray-100 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300 transition duration-300 ease-in-out"
+              value={password()}
+              onInput={(e) => setPassword(e.currentTarget.value)}
+              placeholder="Password"
+              required
             />
-            <label class="text-gray-600 dark:text-gray-200" for="isActive">Is Active</label>
-          </p>
+            <button
+              type="button"
+              class="absolute inset-y-0 right-0 px-3 py-2 flex items-center text-gray-600 dark:text-gray-300 hover:text-gray-400 transition duration-300 ease-in-out"
+              onClick={() => setShowPassword(!showPassword())}
+              aria-label={showPassword() ? "Hide Password" : "Show Password"}
+            >
+              {showPassword() ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M13.875 18.825A5 5 0 0 1 5.823 10.8m-.634-1.638A9.88 9.88 0 0 1 12 4c4.572 0 8.573 3.043 10.568 7.27a9.88 9.88 0 0 1-2.206 2.848" />
+                  <path d="M15 9a3 3 0 0 0-3 3m0 3a3 3 0 0 0 3-3m-3 0 1.5 1.5" />
+                  <line x1="2" y1="2" x2="22" y2="22" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
-        <div class="my-4 flex gap-2">
-          <label class="block text-sm font-medium text-gray-600 dark:text-gray-200" for="isClassTeacher">Class Teacher Status: </label>
-          <p class="flex gap-1">
+
+        <div class="mb-4">
+          <label class="block text-sm font-medium mb-2 text-gray-600 dark:text-gray-200" for="confirmPassword"></label>
+          <div class="relative">
             <input
-              id="isClassTeacher"
-              type="checkbox"
-              checked={isClassTeacher()}
-              onChange={(e) => setIsClassTeacher(e.currentTarget.checked)}
+              id="confirmPassword"
+              type={showConfirmPassword() ? "text" : "password"}
+              class="block w-full px-4 py-2 text-gray-700 dark:text-gray-100 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300 transition duration-300 ease-in-out"
+              value={confirmPassword()}
+              onInput={(e) => setConfirmPassword(e.currentTarget.value)}
+              placeholder="Confirm Password"
+              required
             />
-            <label class="text-gray-600 dark:text-gray-200" for="isClassTeacher">Is Class Teacher</label>
-          </p>
+            <button
+              type="button"
+              class="absolute inset-y-0 right-0 px-3 py-2 flex items-center text-gray-600 dark:text-gray-300 hover:text-gray-400 transition duration-300 ease-in-out"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword())}
+              aria-label={showConfirmPassword() ? "Hide Confirm Password" : "Show Confirm Password"}
+            >
+              {showConfirmPassword() ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M13.875 18.825A5 5 0 0 1 5.823 10.8m-.634-1.638A9.88 9.88 0 0 1 12 4c4.572 0 8.573 3.043 10.568 7.27a9.88 9.88 0 0 1-2.206 2.848" />
+                  <path d="M15 9a3 3 0 0 0-3 3m0 3a3 3 0 0 0 3-3m-3 0 1.5 1.5" />
+                  <line x1="2" y1="2" x2="22" y2="22" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
-        <div class="flex justify-end">
+
+        <div class="my-4 grid grid-cols-2 gap-4">
+          <div>
+            <label class="block text-sm font-medium text-gray-600 dark:text-gray-200" for="isActive">Active Status: </label>
+            <div class="flex items-center gap-2">
+              <input
+                id="isActive"
+                type="checkbox"
+                checked={isActive()}
+                onChange={(e) => setIsActive(e.currentTarget.checked)}
+                class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out"
+              />
+              <label class="text-gray-600 dark:text-gray-200" for="isActive">Is Active</label>
+            </div>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 dark:text-gray-200" for="isClassTeacher">Class Teacher Status: </label>
+            <div class="flex items-center gap-2">
+              <input
+                id="isClassTeacher"
+                type="checkbox"
+                checked={isClassTeacher()}
+                onChange={(e) => setIsClassTeacher(e.currentTarget.checked)}
+                class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out"
+              />
+              <label class="text-gray-600 dark:text-gray-200" for="isClassTeacher">Is Class Teacher</label>
+            </div>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 dark:text-gray-200" for="isAccountant">Accountant Status: </label>
+            <div class="flex items-center gap-2">
+              <input
+                id="isAccountant"
+                type="checkbox"
+                checked={isAccountant()}
+                onChange={(e) => setIsAccountant(e.currentTarget.checked)}
+                class="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out"
+              />
+              <label class="text-gray-600 dark:text-gray-200" for="isAccountant">Is Accountant</label>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex justify-end mt-6">
           <button
             class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2"
             onClick={props.onClose}
