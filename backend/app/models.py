b224207c82b2e6249f28eeb41adfef1f7fd8d895
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-02 14:30:51 +0200

    new alembic migrations
    Former-commit-id: d697abea4341bbb2d2f3169e0626920e5fdfa268

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,7 +8,7 @@ from sqlmodel import Field, Relationship, SQLModel
 class UserBase(SQLModel):
     email: EmailStr = Field(unique=True, index=True, max_length=255)
     is_active: bool = True
-    is_class_teacher: bool | None = False
+    is_class_teacher: bool = False
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
 
