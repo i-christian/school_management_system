a1462816db453c193d8fa98b00f17d61f2154165
honorifia <40751841+honorifia@users.noreply.github.com>
2024-09-03 08:57:45 +0200

    Added togglethem to the nav bar when in mobile view, that is for screens less than 780px, themetoggle is hidden from header and goes into sidebar, also, added configuration md2 which is equal to 780px in tailwinf.config.js for screen sizes.
    Former-commit-id: 91983bc5d79e9da3593aee4212ad5f02ec099afc

--- frontend/src/components/home/Header.tsx
+++ frontend/src/components/home/Header.tsx
@@ -3,17 +3,17 @@ import Nav from "./Nav";
 import { navbarElements } from "../../context";
 import ThemeToggle from "../theme/ThemeToggle";
 
-
 const Header: Component = () => {
   return (
     <>
       <header class="fixed top-0 left-0 w-full shadow-md z-50 bg-inherit backdrop-filter backdrop-blur-3xl backdrop-brightness-100 backdrop-contrast-100 px-5 flex justify-between items-center">
         <Nav navbarElements={navbarElements} />
-        <ThemeToggle />
+        <div class="max-md2:hidden flex items-center justify-center my-[10px] pb-[13px]">
+          <ThemeToggle />
+        </div>
       </header>
     </>
   );
 };
 
 export default Header;
-
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      md2: "780px",
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1920px",
+      // => @media (min-width: 1920px) { ... }
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      }
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
