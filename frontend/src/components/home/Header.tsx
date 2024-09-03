aa8b2d6d472d89840aea18d0cbf31538628b0e40
honorifia <40751841+honorifia@users.noreply.github.com>
2024-09-03 11:15:00 +0200

    Changed background color of navbar in light mode to more white transparent background than previous

--- frontend/src/components/home/Header.tsx
+++ frontend/src/components/home/Header.tsx
@@ -8,7 +8,7 @@ const Header: Component = () => {
     <>
       <header class="fixed top-0 left-0 w-full shadow-md z-50 bg-inherit backdrop-filter backdrop-blur-3xl backdrop-brightness-100 backdrop-contrast-100 px-5 flex justify-between items-center">
         <Nav navbarElements={navbarElements} />
-        <div class="max-md2:hidden flex items-center justify-center my-[10px] pb-[13px]">
+        <div class="max-md2:hidden flex items-center justify-center my-[10px] pb-[20px]">
           <ThemeToggle />
         </div>
       </header>
