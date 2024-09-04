b018b3cc50926cc9f40d7963161b4b538f3bd8d8
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-04 15:31:41 +0200

    Update Students list table
    - Consistent table UI through the application

--- frontend/src/components/admin/students/Students.tsx
+++ frontend/src/components/admin/students/Students.tsx
@@ -58,7 +58,7 @@ const Students: Component = () => {
   };
 
   return (
-    <section class="p-6">
+    <section class="flex flex-col p-6">
       <h2 class="text-2xl font-bold mb-4 text-gray-700 dark:text-gray-200">Students</h2>
       <div class="flex justify-between items-center mb-4">
         <button
@@ -78,45 +78,51 @@ const Students: Component = () => {
           <div class="mb-8">
             <h3 class="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">{className}</h3>
             <div class="overflow-x-auto">
-              <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                <thead class="bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-200">
+              <table class="min-w-full bg-white dark:bg-gray-800">
+                <thead>
                   <tr>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">#</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Last Name</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">First Name</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Middle Name</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Contact Phone</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Actions</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">#</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Last Name</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">First Name</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Middle Name</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Contact Phone</th>
+                    <th scope="col" class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Actions</th>
                   </tr>
                 </thead>
-                <tbody class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
+                <tbody>
                   <For each={studentsInClass}>
                     {(studentItem, index) => (
-                      <tr class="hover:bg-gray-50 dark:hover:bg-gray-800">
-                        <td class="px-6 py-4 text-sm font-medium text-gray-900 dark:text-gray-100">{index() + 1}</td>
-                        <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">{studentItem.last_name}</td>
-                        <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">{studentItem.first_name}</td>
-                        <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">{studentItem.middle_name || "N/A"}</td>
-                        <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">{studentItem.contact || "N/A"}</td>
-                        <td class="px-6 py-4 text-sm font-medium text-right">
-                          <button
-                            class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300"
-                            onClick={() => {
-                              setSelectedStudentId(studentItem.id);
-                              setModalType("edit");
-                            }}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            class="ml-4 text-red-600 hover:text-red-900 dark:text-red-400 dark:hover:text-red-300"
-                            onClick={() => {
-                              setSelectedStudentId(studentItem.id);
-                              setModalType("delete");
-                            }}
-                          >
-                            Delete
-                          </button>
+                      <tr class={index() % 2 === 0 ? "bg-gray-100 dark:bg-gray-700" : ""}>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">{index() + 1}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-gray-800 dark:text-gray-100">{studentItem.last_name}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-gray-800 dark:text-gray-100">{studentItem.first_name}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-gray-800 dark:text-gray-100">{studentItem.middle_name || "N/A"}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-gray-800 dark:text-gray-100">{studentItem.contact || "N/A"}</td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <div class="flex items-center justify-end space-x-2">
+                            <button
+                              class="p-2 rounded-md text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-100"
+                              onClick={() => {
+                                setSelectedStudentId(studentItem.id);
+                                setModalType("edit");
+                              }}
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                              </svg>
+                            </button>
+                            <button
+                              class="p-2 ml-2 rounded-md text-red-600 dark:text-red-400 hover:text-red-800 dark:hover:text-red-300"
+                              onClick={() => {
+                                setSelectedStudentId(studentItem.id);
+                                setModalType("delete");
+                              }}
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.111 48.111 0 0 0-7.5 0" />
+                              </svg>
+                            </button>
+                          </div>
                         </td>
                       </tr>
                     )}
