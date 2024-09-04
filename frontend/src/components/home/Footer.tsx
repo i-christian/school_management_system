4fc6875b6d0203734e97369682a87945044c6e86
honorifia <40751841+honorifia@users.noreply.github.com>
2024-09-04 07:48:29 +0200

    Home done for now
    Former-commit-id: f516972ce9f75561fb3385c418d2d0912bbead98

--- frontend/src/components/home/Footer.tsx
+++ frontend/src/components/home/Footer.tsx
@@ -1,20 +1,33 @@
-import { Component } from 'solid-js';
+import { Component } from "solid-js";
+import { schoolName } from "../../context";
 
 const Footer: Component = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer class="bg-inherit" id="contact">
-      <hr class="h-px my-6 bg-gray-200 border-none dark:bg-gray-700" />
+    <footer class="bg-white dark:bg-gray-900" id="contact">
+      <hr class="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700" />
       <div class="container p-6 mx-auto text-center">
-        <h3 class="text-gray-700 uppercase dark:text-white">Contact</h3>
-        <span class="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+265 886 8965</span>
-        <span class="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">example@email.com</span>
+        <h3 class="text-gray-800 uppercase dark:text-white">Contact</h3>
+        <span class="block mt-2 text-sm text-gray-800 dark:text-gray-400 hover:underline">
+          +265 886 8965
+        </span>
+        <span class="block mt-2 text-sm text-gray-800 dark:text-gray-400 hover:underline">
+          example@email.com
+        </span>
 
-        <p class="mt-5 text-center text-gray-500 dark:text-gray-400">© School Name {currentYear} - All rights reserved</p>
+        <p class="mt-5 text-center text-gray-700 dark:text-gray-400">
+          ©
+          <span>
+            <span class=" p-3 max-sm:hidden min-[1060px]:hidden">{`${schoolName[0].name}`}</span>
+            <span class=" p-3 max-[1060px]:hidden">{`${schoolName[0].full}`}</span>
+            <span class=" p-3 sm:hidden">{`${schoolName[0].short}`}</span>
+          </span>{" "}
+          {currentYear} - All rights reserved
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
