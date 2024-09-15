248aa5a3587e2e3c1588c78607ed42d8c3ef720f
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-15 23:28:16 +0200

    update student test
    Former-commit-id: 9489bdec2bc8d67679fb5bb3457386d0e0c65a41

--- backend/app/tests/crud/test_student.py
+++ backend/app/tests/crud/test_student.py
@@ -38,22 +38,8 @@ def test_update_student(db: Session) -> None:
 def test_delete_student(db: Session) -> None:
     student = create_test_student(db)
     deleted_student = crud.delete_student(session=db, student_id=student.id)
+    assert deleted_student
     assert deleted_student.id == student.id
 
     student_2 = db.get(Student, student.id)
     assert student_2 is None
-
-
-def test_update_non_existent_student(db: Session) -> None:
-    non_existent_student_id = uuid.uuid4()
-    student_in_update = StudentUpdate(first_name="New Name")
-
-    non_existent_student = db.get(Student, non_existent_student_id)
-    assert non_existent_student is None
-
-    try:
-        crud.update_student(
-            session=db, db_student=non_existent_student, student_in=student_in_update
-        )
-    except AttributeError:
-        assert True
