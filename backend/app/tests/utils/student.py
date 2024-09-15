fb0142440c60ae30764a07ced17affa406bfbe90
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-15 23:18:47 +0200

    add test student crud

--- backend/app/tests/utils/student.py
+++ backend/app/tests/utils/student.py
@@ -1,44 +1,20 @@
-import random
+import uuid
 
 from sqlmodel import Session
 
 from app import crud
-from app.models import ClassFormCreate, Student, StudentCreate
-from app.tests.utils.user import create_random_user
-from app.tests.utils.utils import random_lower_string
+from app.models import Student, StudentCreate
 
 
-def create_random_student(db: Session) -> Student:
-    # Create a random user
-    user = create_random_user(db)
-    owner_id = user.id
-    assert owner_id is not None
-
-    # Create a random class form
-    class_form_in = ClassFormCreate(name=random_lower_string())
-    class_form = crud.create_class_form(session=db, class_form_in=class_form_in)
-    form_id = class_form.id
-
-    # Generate random details for the student
-    first_name = random_lower_string()
-    last_name = random_lower_string()
-    middle_name = random_lower_string()
-    contact = f"12345{random.randint(10000, 99999)}"
-    fees = 600000.0
-    class_teacher_remark = random_lower_string()
-    head_teacher_remark = random_lower_string()
-
-    # Create the StudentCreate object with random details
+def create_test_student(db: Session) -> Student:
     student_in = StudentCreate(
-        first_name=first_name,
-        middle_name=middle_name,
-        last_name=last_name,
-        contact=contact,
-        form_id=form_id,
-        fees=fees,
-        class_teacher_remark=class_teacher_remark,
-        head_teacher_remark=head_teacher_remark,
+        first_name="Test",
+        middle_name="Middle",
+        last_name="Student",
+        contact="123456789",
+        form_id=uuid.uuid4(),
+        fees=500.0,
+        class_teacher_remark="Good",
+        head_teacher_remark="Needs Improvement",
     )
-
-    # Create the student in the database
-    return crud.create_student(session=db, student_in=student_in, owner_id=owner_id)
+    return crud.create_student(session=db, student_in=student_in, owner_id=uuid.uuid4())
