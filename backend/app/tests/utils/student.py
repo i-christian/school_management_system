ffa3a58f97f2278234053bed6b425c174bab7604
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-15 23:54:19 +0200

    fixed failing tests
    Former-commit-id: a8523b43d60395944dd72dac29747f41bf2c0b8f

--- backend/app/tests/utils/student.py
+++ backend/app/tests/utils/student.py
@@ -4,9 +4,13 @@ from sqlmodel import Session
 
 from app import crud
 from app.models import Student, StudentCreate
+from app.tests.utils.user import create_random_user
 
 
 def create_test_student(db: Session) -> Student:
+    user = create_random_user(db)
+    owner_id = user.id
+    assert owner_id is not None
     student_in = StudentCreate(
         first_name="Test",
         middle_name="Middle",
@@ -17,4 +21,4 @@ def create_test_student(db: Session) -> Student:
         class_teacher_remark="Good",
         head_teacher_remark="Needs Improvement",
     )
-    return crud.create_student(session=db, student_in=student_in, owner_id=uuid.uuid4())
+    return crud.create_student(session=db, student_in=student_in, owner_id=owner_id)
