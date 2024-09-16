4b21ac6c138700e4cfac822c28b0af2cd0254931
Christian <111076892+i-christian@users.noreply.github.com>
2024-09-16 16:16:36 +0200

    resolved lint issues

--- backend/app/tests/api/routes/test_students.py
+++ backend/app/tests/api/routes/test_students.py
@@ -2,7 +2,6 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session
 
 from app.core.config import settings
-from app.models import StudentCreate
 from app.tests.utils.class_form import create_test_class_form
 
 # from app.tests.utils.student import create_test_student
