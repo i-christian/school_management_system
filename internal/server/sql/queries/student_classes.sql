54e401046820c6319550e07f3e9e565632c9e037
christian <111076892+i-christian@users.noreply.github.com>
2025-01-15 12:46:22 +0200

    feat: implement `student_classes` CRUD operations

---
+++ internal/server/sql/queries/student_classes.sql
@@ -0,0 +1,18 @@
+-- name: CreateStudentClasses :one
+INSERT INTO student_classes (student_id, class_id)
+VALUES ($1, $2) RETURNING *;
+
+-- name: GetStudentClasses :one
+SELECT * FROM student_classes WHERE student_class_id = $1;
+
+-- name: ListStudentClasses :many
+SELECT * FROM student_classes;
+
+-- name: EditStudentClasses :exec
+UPDATE student_classes
+SET student_id = COALESCE($2, student_id),
+class_id = COALESCE($3, class_id)
+WHERE student_class_id = $1;
+
+-- name: DeleteStudentClasses :exec
+DELETE FROM student_classes WHERE student_class_id = $1;
