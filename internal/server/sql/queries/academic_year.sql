42366c397c62ef7aa69ed47d348300bdd39d97c7
christian <111076892+i-christian@users.noreply.github.com>
2025-01-15 02:13:57 +0200

    fix: add a missing semicolon to the end of an sql statement

--- internal/server/sql/queries/academic_year.sql
+++ internal/server/sql/queries/academic_year.sql
@@ -18,7 +18,7 @@ WHERE academic_year_id = $4;
 
 -- name: DeleteAcademicYear :exec
 DELETE FROM academic_year
-WHERE academic_year = $1;
+WHERE academic_year_id = $1;
 
 -- name: CreateTerm :one
 INSERT INTO term (academic_year_id, name, start_date, end_date) 
@@ -35,10 +35,10 @@ SELECT * FROM term WHERE term_id = $1;
 UPDATE term 
 SET academic_year_id = COALESCE($1, academic_year_id),
 name = COALESCE($2, name),
-start_year = COALESCE($3, start_year),
-end_year = COALESCE($4, end_year)
+start_date = COALESCE($3, start_date),
+end_date = COALESCE($4, end_date)
 WHERE term_id = $5;
 
 -- name: DeleteTerm :exec
 DELETE FROM term
-WHERE term_id = $1
+WHERE term_id = $1;
