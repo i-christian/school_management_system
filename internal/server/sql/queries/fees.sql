20dd7b97355242b27c0e5edb724571e6d540220a
christian <111076892+i-christian@users.noreply.github.com>
2025-01-16 00:35:11 +0200

    improve `fees` table using join statements to return usable data

--- internal/server/sql/queries/fees.sql
+++ internal/server/sql/queries/fees.sql
@@ -4,11 +4,41 @@ VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
 -- name: GetStudentFeesRecord :one
-SELECT * FROM fees
-WHERE student_id = $1;
+SELECT
+    fees.fees_id,
+    students.last_name AS LastName,
+    students.first_name AS FirstName,
+    term.name AS AcademicTerm,
+    classes.name AS ClassName,
+    fees.required AS TutionAmount,
+    fees.paid AS PaidAmount,
+    fees.status
+FROM fees
+INNER JOIN students
+    ON fees.student_id = students.student_id
+INNER JOIN term
+    ON fees.term_id = term.term_id
+INNER JOIN classes
+    ON fees.class_id = classes.class_id
+WHERE students.student_id = $1;
 
 -- name: ListStudentFeesRecords :many
-SELECT * FROM fees;
+SELECT
+    fees.fees_id,
+    students.last_name AS LastName,
+    students.first_name AS FirstName,
+    term.name AS AcademicTerm,
+    classes.name AS ClassName,
+    fees.required AS TutionAmount,
+    fees.paid AS PaidAmount,
+    fees.status
+FROM fees
+INNER JOIN students
+    ON fees.student_id = students.student_id
+INNER JOIN term
+    ON fees.term_id = term.term_id
+INNER JOIN classes
+    ON fees.class_id = classes.class_id;
 
 -- name: EditFeesRecord :exec
 UPDATE fees
