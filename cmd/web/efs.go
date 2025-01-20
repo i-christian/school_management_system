9ba6cf7d3d65e228bf76da745ce265b6fc4ed86d
christian <111076892+i-christian@users.noreply.github.com>
2025-01-20 02:48:59 +0200

    feat: create sidebar with user section, settings, and logout functionality
    - Added a sidebar with navigation links for Home, Insights, Users, Students, Grades, and Fees.
    - Included a user section with a username placeholder.
    - Styled logout and settings buttons with hover effects and appropriate colors.
    - Implemented responsive design for mobile and desktop views.

--- cmd/web/efs.go
+++ cmd/web/efs.go
@@ -1,6 +1,14 @@
 package web
 
-import "embed"
+import (
+	"embed"
+	"os"
+)
 
 //go:embed "assets"
 var Files embed.FS
+
+// Define the project name
+func SchoolName() string {
+	return os.Getenv("PROJECT_NAME")
+}
