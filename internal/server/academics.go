8b8296f07287341745015f245330da3ce1aaa76c
christian <111076892+i-christian@users.noreply.github.com>
2025-01-29 21:16:22 +0200

    feat: implement edit academic year handler function

--- internal/server/academics.go
+++ internal/server/academics.go
@@ -6,6 +6,7 @@ import (
 
 	"school_management_system/internal/database"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
@@ -49,4 +50,55 @@ func (s *Server) CreateAcademicYear(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) ListAcademicYears(w http.ResponseWriter, r *http.Request) {
+	_, err := s.queries.ListAcademicYear(r.Context())
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, "could not fetch academic year list")
+	}
+}
+
+func (s *Server) EditAcademicYear(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		writeError(w, http.StatusMethodNotAllowed, "method not allowed")
+	}
+
+	academic_year_id, err := uuid.Parse(r.PathValue("id"))
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, "failed to parse id")
+	}
+
+	if err := r.ParseForm(); err != nil {
+		writeError(w, http.StatusUnprocessableEntity, "failed to parse form")
+		return
+	}
+
+	name := r.FormValue("name")
+	start := r.FormValue("start")
+	end := r.FormValue("end")
+
+	// validate form
+	if name == "" || start == "" || end == "" {
+		writeError(w, http.StatusBadRequest, "all fields are required")
+	}
+	startDate, err := time.Parse(time.DateOnly, start)
+	if err != nil {
+		writeError(w, http.StatusBadGateway, "failed to parse start date")
+		return
+	}
+
+	endDate, err := time.Parse(time.DateOnly, end)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "failed to parse end date")
+	}
+
+	params := database.EditAcademicYearParams{
+		Name:           name,
+		StartDate:      pgtype.Date{Time: startDate, Valid: true},
+		EndDate:        pgtype.Date{Time: endDate, Valid: true},
+		AcademicYearID: academic_year_id,
+	}
+
+	err = s.queries.EditAcademicYear(r.Context(), params)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, "internal server error")
+	}
 }
