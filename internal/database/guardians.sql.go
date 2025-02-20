b60fc3107129ff710df9a7e9804bbd138388fee2
christian <111076892+i-christian@users.noreply.github.com>
2025-02-20 23:03:19 +0200

    refactor: fix active search functionality on guardians page

--- internal/database/guardians.sql.go
+++ internal/database/guardians.sql.go
@@ -24,8 +24,8 @@ func (q *Queries) DeleteGuardian(ctx context.Context, guardianID uuid.UUID) erro
 const getAllStudentGuardianLinks = `-- name: GetAllStudentGuardianLinks :many
 SELECT
     g.guardian_id,
-    s.last_name AS student_first_name,
-    s.first_name AS student_last_name,
+    s.first_name AS student_first_name,
+    s.last_name AS student_last_name,
     g.guardian_name AS guardian_name,
     g.phone_number_1,
     g.phone_number_2,
--- internal/server/sql/queries/guardians.sql
+++ internal/server/sql/queries/guardians.sql
@@ -26,8 +26,8 @@ GROUP BY student_guardians.guardian_id;
 -- name: GetAllStudentGuardianLinks :many
 SELECT
     g.guardian_id,
-    s.last_name AS student_first_name,
-    s.first_name AS student_last_name,
+    s.first_name AS student_first_name,
+    s.last_name AS student_last_name,
     g.guardian_name AS guardian_name,
     g.phone_number_1,
     g.phone_number_2,
