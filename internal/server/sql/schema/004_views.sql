0a65c57859ca42e8c8054871d96b91b530685d75
christian <111076892+i-christian@users.noreply.github.com>
2025-02-23 14:59:47 +0200

    feat: implement a virtual_classroom view
    The view brings together students, subjects, classes, teachers, and term details.

--- internal/server/sql/schema/004_views.sql
+++ internal/server/sql/schema/004_views.sql
@@ -25,5 +25,31 @@ LEFT JOIN grades g ON s.student_id = g.student_id
                    AND sc.term_id = g.term_id
 GROUP BY s.student_id, s.student_no, s.last_name, s.first_name, s.middle_name, c.name, c.class_id;
 
+CREATE OR REPLACE VIEW virtual_classroom AS
+SELECT
+    st_cl.student_id,
+    s.subject_id,
+    s.name AS subject_name,
+    c.class_id,
+    c.name AS class_name,
+    u.user_id AS teacher_id,
+    u.first_name || ' ' || u.last_name AS teacher_name,
+    t.term_id,
+    t.name AS term_name,
+    t.academic_year_id
+FROM
+    student_classes st_cl
+JOIN
+    assignments a ON st_cl.class_id = a.class_id
+JOIN
+    subjects s ON a.subject_id = s.subject_id
+JOIN
+    classes c ON a.class_id = c.class_id
+JOIN
+    users u ON a.teacher_id = u.user_id
+JOIN
+    term t ON st_cl.term_id = t.term_id;
+
 -- +goose Down
 DROP VIEW student_grades_view;
+DROP VIEW virtual_classroom;
