fdf0342fde8930234864dc189ce42005a305d22c
christian <111076892+i-christian@users.noreply.github.com>
2025-02-28 03:37:34 +0200

    refactor: add new queries in `insights.sql` to get some data insights

--- internal/server/sql/queries/insights.sql
+++ internal/server/sql/queries/insights.sql
@@ -9,3 +9,20 @@ FROM students;
 -- name: GetTotalFeesPaid :one
 SELECT COALESCE(SUM(paid), 0) AS total_fees_paid
 FROM fees;
+
+-- name: GetStudentGenderBreakdown :many
+SELECT gender, COUNT(*) AS total_students
+FROM students
+GROUP BY gender;
+
+-- name: GetTotalGuardians :one
+SELECT COUNT(*) AS total_guardians
+FROM guardians;
+
+-- name: GetAverageGradeScore :one
+SELECT AVG(score) AS average_grade
+FROM grades;
+
+-- name: GetTotalDisciplineRecords :one
+SELECT COUNT(*) AS total_discipline_records
+FROM discipline_records;
