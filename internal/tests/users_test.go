7fceaa738f93ff80399b6291d2fd76776333ca2d
christian <111076892+i-christian@users.noreply.github.com>
2025-03-21 19:53:34 +0200

    test: add `logout` endpoint test

--- internal/tests/users_test.go
+++ internal/tests/users_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -15,7 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRegisterUser(t *testing.T) {
+func TestUserActions(t *testing.T) {
 	ctx := context.Background()
 
 	postgresC, dsn := setupPostgresContainer(t)
@@ -68,6 +67,34 @@ func TestRegisterUser(t *testing.T) {
 	// Send Get to /profile endpoint to get user details
 	req, err = http.NewRequest(http.MethodGet, ts.URL+"/profile", nil)
 	require.NoError(t, err)
+	for _, cookie := range cookieJar.Cookies(req.URL) {
+		req.AddCookie(cookie)
+	}
+
 	resp, err = client.Do(req)
+	defer resp.Body.Close()
+
 	require.Equal(t, http.StatusOK, resp.StatusCode, "Expected 200 OK with user information")
+
+	// Send a Post to /logout endpoint to logout
+	req, err = http.NewRequest(http.MethodPost, ts.URL+"/logout", nil)
+	require.NoError(t, err)
+	for _, cookie := range cookieJar.Cookies(req.URL) {
+		req.AddCookie(cookie)
+	}
+
+	resp, err = client.Do(req)
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusFound, resp.StatusCode, "Expected 302 Found after logout")
+
+	redirectURL, err := resp.Location()
+	require.NoError(t, err, "Expected Location header")
+	redirectReq, err := http.NewRequest(http.MethodGet, redirectURL.String(), nil)
+	require.NoError(t, err)
+	redirectResp, err := client.Do(redirectReq)
+	require.NoError(t, err)
+	defer redirectResp.Body.Close()
+
+	require.Equal(t, http.StatusOK, redirectResp.StatusCode, "Expected 200 OK after redirect")
 }
