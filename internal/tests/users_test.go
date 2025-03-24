13c2222a3d1b57826ff9d2a64c37d2b3ec457ba6
christian <111076892+i-christian@users.noreply.github.com>
2025-03-24 11:42:06 +0200

    tests(integration): refactor user's test

--- internal/tests/users_test.go
+++ internal/tests/users_test.go
@@ -17,7 +17,7 @@ func TestUserActions(t *testing.T) {
 		TestTeardown(t, postgresC)
 	}()
 
-	// Step 1: Log in as the superuser to get the session cookie
+	// Step 1: Log in as the superuser to get session cookie
 	loginReq, cookieJar, err := LoginHelper(t, ts, &LoginInfo{
 		Identifier: os.Getenv("SUPERUSER_PHONE"),
 		Password:   os.Getenv("SUPERUSER_PASSWORD"),
@@ -28,62 +28,67 @@ func TestUserActions(t *testing.T) {
 	defer resp.Body.Close()
 
 	// Step 2: Register a new user
-	newUserData := url.Values{}
-	newUserData.Set("first_name", "Naruto")
-	newUserData.Set("last_name", "Uzumaki")
-	newUserData.Set("phone_number", "1234567890")
-	newUserData.Set("email", "naruto@example.com")
-	newUserData.Set("gender", "M")
-	newUserData.Set("role", "teacher")
+	t.Run("Create a new user", func(t *testing.T) {
+		newUserData := url.Values{}
+		newUserData.Set("first_name", "Naruto")
+		newUserData.Set("last_name", "Uzumaki")
+		newUserData.Set("phone_number", "1234567890")
+		newUserData.Set("email", "naruto@example.com")
+		newUserData.Set("gender", "M")
+		newUserData.Set("role", "teacher")
 
-	// Send POST request to /users/ endpoint
-	req, err := http.NewRequest(http.MethodPost, ts.URL+"/users/", strings.NewReader(newUserData.Encode()))
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req, err := http.NewRequest(http.MethodPost, ts.URL+"/users/", strings.NewReader(newUserData.Encode()))
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	for _, cookie := range cookieJar.Cookies(req.URL) {
-		req.AddCookie(cookie)
-	}
+		for _, cookie := range cookieJar.Cookies(req.URL) {
+			req.AddCookie(cookie)
+		}
 
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
+		resp, err = client.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
 
-	require.Equal(t, http.StatusOK, resp.StatusCode, "Expected 200 OK after creating user")
+		require.Equal(t, http.StatusOK, resp.StatusCode, "Expected 200 OK after creating user")
+	})
 
-	// Send Get to /profile endpoint to get user details
-	req, err = http.NewRequest(http.MethodGet, ts.URL+"/profile", nil)
-	require.NoError(t, err)
-	for _, cookie := range cookieJar.Cookies(req.URL) {
-		req.AddCookie(cookie)
-	}
+	// Send Get to /profile endpoint to get current logged in user details
+	t.Run("User Details", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, ts.URL+"/profile", nil)
+		require.NoError(t, err)
+		for _, cookie := range cookieJar.Cookies(req.URL) {
+			req.AddCookie(cookie)
+		}
 
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
+		resp, err = client.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
 
-	require.Equal(t, http.StatusOK, resp.StatusCode, "Expected 200 OK with user information")
+		require.Equal(t, http.StatusOK, resp.StatusCode, "Expected 200 OK with user information")
+	})
 
 	// Send a Post to /logout endpoint to logout
-	req, err = http.NewRequest(http.MethodPost, ts.URL+"/logout", nil)
-	require.NoError(t, err)
-	for _, cookie := range cookieJar.Cookies(req.URL) {
-		req.AddCookie(cookie)
-	}
+	t.Run("Logout", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, ts.URL+"/logout", nil)
+		require.NoError(t, err)
+		for _, cookie := range cookieJar.Cookies(req.URL) {
+			req.AddCookie(cookie)
+		}
 
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	defer resp.Body.Close()
+		resp, err = client.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
 
-	require.Equal(t, http.StatusFound, resp.StatusCode, "Expected 302 Found after logout")
+		require.Equal(t, http.StatusFound, resp.StatusCode, "Expected 302 Found after logout")
 
-	redirectURL, err := resp.Location()
-	require.NoError(t, err, "Expected Location header")
-	redirectReq, err := http.NewRequest(http.MethodGet, redirectURL.String(), nil)
-	require.NoError(t, err)
-	redirectResp, err := client.Do(redirectReq)
-	require.NoError(t, err)
-	defer redirectResp.Body.Close()
+		redirectURL, err := resp.Location()
+		require.NoError(t, err, "Expected Location header")
+		redirectReq, err := http.NewRequest(http.MethodGet, redirectURL.String(), nil)
+		require.NoError(t, err)
+		redirectResp, err := client.Do(redirectReq)
+		require.NoError(t, err)
+		defer redirectResp.Body.Close()
 
-	require.Equal(t, http.StatusOK, redirectResp.StatusCode, "Expected 200 OK after redirect")
+		require.Equal(t, http.StatusOK, redirectResp.StatusCode, "Expected 200 OK after redirect")
+	})
 }
